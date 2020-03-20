426ef21ac0ee32d927acf10e13094ee49c542244
Iuri <theozzybr@gmail.com>
2020-03-20 11:42:14 -0300

    coronavirus added

--- botlemos/src/core.py
+++ botlemos/src/core.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-from stonks import cotar, frasear
+from stonks import cotar, frasear, coronar
 TELEGRAM_TOKEN = "935141102:AAE5ssMgzHoQ95yV7WvO304EKKcBSRtzMdQ"
 url = 'https://economia.awesomeapi.com.br/all/USD-BRL'
 
@@ -19,6 +19,17 @@ def dolar(update, context):
     context.bot.send_photo(chat_id=chat_id, photo=open('cotacao.png', 'rb'))
 
 
+def corona(update, context):
+    chat_id = update.message.chat_id
+    corno = update.message.from_user.first_name
+    coronar(corno)
+    frase = frasear()
+    update.message.reply_text(
+        '{} - Rafael Lemos'.format(frase))
+    context.bot.send_photo(chat_id=chat_id, photo=open('coronafofoelfinal.png', 'rb'))
+    
+
+
 def main():
 
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
@@ -27,8 +38,10 @@ def main():
 
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
     updater.dispatcher.add_handler(CommandHandler('dolar', dolar))
+    updater.dispatcher.add_handler(CommandHandler('corona', corona))
 
     updater.start_polling()
+
 
     updater.idle()
 
--- botlemos/src/stonks.py
+++ botlemos/src/stonks.py
@@ -29,6 +29,50 @@ def cotar(corno):
 
     image.save('cotacao.png')
 
+def coronar(corno):
+    coronabrurl = 'https://coronavirus-19-api.herokuapp.com/countries/brazil'
+    cbr = requests.get(coronabrurl)
+    data = cbr.json()
+    casosbr = float(data['cases'])
+    acasosbr = float(data['active'])
+    mortosbr = float(data['deaths'])
+    mortosbr = float(data['deaths'])
+
+    coronaauurl = 'https://coronavirus-19-api.herokuapp.com/countries/australia'
+    cau = requests.get(coronaauurl)
+    data = cau.json()
+    casosau = float(data['cases'])
+    acasosau = float(data['active'])
+    mortosau = float(data['deaths'])
+
+    image = Image.open('coronafofoel.png')
+
+    font_type = ImageFont.truetype('impact.ttf', 30)
+    font_type2 = ImageFont.truetype('impact.ttf', 30)
+
+    draw = ImageDraw.Draw(image)
+
+    draw.text(xy=(15, 100), text='{} É CORNO'.format(corno), fill=(255, 255, 255),
+              font=font_type2, stroke_fill=2, stroke_width=2)
+              #BRAZIL
+    draw.text(xy=(280, 15), text='CASOS: {}'.format(round(casosbr)), fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+    draw.text(xy=(280, 50), text='ATIVOS: {}'.format(round(acasosbr)), fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+    draw.text(xy=(280, 85), text='MORTOS: {}'.format(round(mortosbr)), fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+              #AUSTRALIA
+    draw.text(xy=(280, 260), text='CASOS: {}'.format(round(casosau)), fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+    draw.text(xy=(280, 295), text='ATIVOS: {}'.format(round(acasosau)), fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+    draw.text(xy=(280, 330), text='MORTOS: {}'.format(round(mortosau)), fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+
+    draw.text(xy=(100, 470), text='FIDELIZA NO LIKE, FAVELA VENCEU', fill=(255, 255, 255),
+              font=font_type, stroke_fill=2, stroke_width=2)
+    image.save('coronafofoelfinal.png')
+
 
 def frasear():
 
