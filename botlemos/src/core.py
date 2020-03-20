49e82a6983bfbc41c9062bd0dee4b70c869e3945
Iuri <theozzybr@gmail.com>
2020-03-20 14:54:42 -0300

    safe token

--- botlemos/src/core.py
+++ botlemos/src/core.py
@@ -1,7 +1,7 @@
+import telegram.ext
+from data import TELEGRAM_TOKEN
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 from stonks import cotar, frasear, coronar
-TELEGRAM_TOKEN = "935141102:AAE5ssMgzHoQ95yV7WvO304EKKcBSRtzMdQ"
-url = 'https://economia.awesomeapi.com.br/all/USD-BRL'
 
 
 def hello(update, context):
@@ -26,14 +26,13 @@ def corona(update, context):
     frase = frasear()
     update.message.reply_text(
         '{} - Rafael Lemos'.format(frase))
-    context.bot.send_photo(chat_id=chat_id, photo=open('coronafofoelfinal.png', 'rb'))
-    
+    context.bot.send_photo(chat_id=chat_id, photo=open(
+        'coronafofoelfinal.png', 'rb'))
 
 
 def main():
 
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
-
     dispatcher = updater.dispatcher
 
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
@@ -41,7 +40,6 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('corona', corona))
 
     updater.start_polling()
-
 
     updater.idle()
 
